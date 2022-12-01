4b0a5d95d52a4363b1999396c8f544830606a794
Hossein-AliHosseini <themmdhossein@gmail.com>
2022-12-01 20:44:39 +0330

    Add compiler.py

--- compiler.py
+++ compiler.py
@@ -1,4 +1,20 @@
-class Compiler:
+from scanner import Scanner
 
+
+class Compiler:
     def __init__(self):
+        self.scanner = Scanner("input.txt")
+
+    def scan(self):
+        while (True):
+            if not self.scanner.get_next_token():
+                break
+        self.scanner.tokens.write_to_file()
+        self.scanner.errors.write_to_file()
+        self.scanner.symbol_table.write_to_file()
+
+    def compile(self):
         pass
+
+compiler = Compiler()
+compiler.scan()
