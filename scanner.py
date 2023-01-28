2cd557bee225c28fba9df64d2166efbf13753b88
Hossein-AliHosseini <themmdhossein@gmail.com>
2023-01-29 02:47:04 +0330

    Modify scanner.py

--- scanner.py
+++ scanner.py
@@ -29,58 +29,43 @@ class Scanner:
             self.end_of_file = True
         return c
 
-    def rewind_char(self):
-        self.is_rewinded = True
 
     def is_whitespace(self, char):
-        return char == " " or char == "\t" or char == "\n" or char == "\r" or char == "\v" or char == "\f"
+        return char in [" ", "\t", "\n", "\r", "\v", "\f"]
+
 
     def get_next_token(self):
         buffer = ""
         last_char = None
         self.dfa.reset()
-        while not self.dfa.is_final():
+        while True:
             last_char = self.get_char()
             if self.end_of_file:
                 return "EOF", '$', self.line_no
             if last_char is None:
                 break
-            # if last_char == '':
-            #     last_char = '\n'
-            if last_char != "\n":
-                buffer += last_char
+            appended = "" if last_char == "\n" else last_char
+            buffer += appended
             if not self.dfa.can_move_current_state(last_char):
                 self.errors.add_error((buffer, "Invalid input"), self.line_no)
-                if last_char == "\n":
-                    self.line_no += 1
+                self.line_no += 1 if last_char == "\n" else 0
                 return self.get_next_token()
-                # return True
+            if self.dfa.is_final():
+                break
         if self.dfa.is_look_ahead():
-            if last_char != '\n':
-                buffer = buffer[:-1]
-            self.rewind_char()
+            buffer = buffer[:-1] if last_char != "\n" else buffer
+            self.is_rewinded = True
             last_char = None
-        if last_char == "\n":
-            self.line_no += 1
-        if self.dfa.is_error():
-            self.errors.add_error((buffer, self.dfa.get_error_type()), self.line_no)
-            return self.get_next_token()
-            # return True
+        self.line_no += 1 if last_char == "\n" else 0
         if not self.dfa.is_final():
             if self.dfa.get_type() == "COMMENT":
                 message = buffer[:min(7, len(buffer))]
-                if len(buffer) > 7:
-                    message += "..."
+                message += "..." if len(buffer) > 7 else ""
                 self.errors.add_error((message, "Unclosed comment"), self.line_no)
             return self.get_next_token()
-            # return False
-        if buffer in self.symbol_table.keywords:
-            token_type = "KEYWORD"
-        else:
-            token_type = self.dfa.get_type()
-        # if token_type != 'WHITESPACE' and token_type != 'COMMENT':
+        if self.dfa.is_error():
+            self.errors.add_error((buffer, self.dfa.get_error_type()), self.line_no)
+            return self.get_next_token()
+        token_type = "KEYWORD" if buffer in self.symbol_table.keywords else self.dfa.get_type()
         self.tokens.add_token((token_type, buffer), self.line_no)
         return token_type, buffer, self.line_no
-        # if token_type == 'ID':
-        #     self.symbol_table.add_symbol(buffer)
-        # return True
